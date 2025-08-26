76058e77efb67ab1d66098eab799dcbb60e98265
Sailesh Sharma <sailesh.sharma@gmail.com>
2025-08-26 22:00:18 +0800

    Fix import path for App in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ApolloProvider } from "@apollo/client";
 import client from "./apolloClient";
-import App from "./App";
+import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
